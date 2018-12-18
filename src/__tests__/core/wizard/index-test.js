c3331ef520a8fdd5f56e1bd44cfeeb86b5032a63
sean <sean.large1@gmail.com>
2018-12-17 21:51:20 -0500

    add in one unit test

---
+++ src/__tests__/core/wizard/index-test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Wizard from '../../../core/components/wizard/index';
+import sinon from 'sinon';
+import enzyme from 'enzyme';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    const handleOnComplete = sinon.spy();
+    const wizardContext = {
+        from: {
+            name: "Sean Large",
+                street: "",
+                city: "",
+                state: "",
+                zip: ""
+        },
+        to: {
+            name: "",
+                street: "",
+                city: "",
+                state: "",
+                zip: ""
+        },
+        weight: null,
+            shippingOption: ""
+    };
+
+    ReactDOM.render(<Wizard header={() => {return "some type of header"}}
+                            wizardContext={wizardContext}
+                            steps={["Sender Address", "Receiver Address", "Weight", "Shipping Options", "Confirmation"]}
+                            onComplete={() => handleOnComplete()}/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
