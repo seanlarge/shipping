4e4a58580b18e72885523e04535b9cff5b375e68
sean <sean.large1@gmail.com>
2018-12-18 06:17:13 -0500

    unit test

--- src/__tests__/core/wizard/index-test.js
+++ src/__tests__/core/wizard/index-test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Wizard from '../../../core/components/wizard/index';
 import sinon from 'sinon';
-import enzyme from 'enzyme';
+import {shallow, mount} from 'enzyme';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
